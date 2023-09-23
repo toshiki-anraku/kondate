0b7bb445550b622f5ced73940196542acc801150
安樂星輝 <toshiki.a1122@gmail.com>
2023-09-23 10:16:21 +0900

    文言修正

--- src/app/[recipeId]/page.tsx
+++ src/app/[recipeId]/page.tsx
@@ -70,7 +70,7 @@ export default function Recipe() {
         className="p-2 bg-blue-500 text-white rounded hover:bg-blue-600 mb-4"
         onClick={handleResuggest}
       >
-        再提案
+        同じ食材・ジャンルで別のレシピを提案
       </button>
       {/* 履歴を見るリンク */}
       <Link href="/history" passHref legacyBehavior>
